75ab4192924f633871a659994db666861c4b0bd2
shwetashardul <svs28@njit.edu>
2024-03-02 21:34:30 -0500

    Added Plugins

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,5 @@
-from app.commands import CommandHandler
-from app.commands.discord import DiscordCommand
-from app.commands.exit import ExitCommand
-from app.commands.goodbye import GoodbyeCommand
-from app.commands.greet import GreetCommand
-from app.commands.menu import MenuCommand
-from app.commands.add import AddCommand
-from app.commands.subtract import SubtractCommand
-from app.commands.multiply import MultiplyCommand
-from app.commands.divide import DivideCommand
+'''from app.commands import CommandHandler
+
 
 class App:
     def __init__(self): # Constructor
@@ -37,7 +29,39 @@ class App:
             if command_name in self.command_handler.commands:
                 self.command_handler.execute_command(command_name, args)
             else:
-                print(f"No such command: {command_name}")
+                print(f"No such command: {command_name}")'''
             
+import pkgutil
+import importlib
+from app.commands import CommandHandler
+from app.commands import Command
+
+class App:
+    def __init__(self): # Constructor
+        self.command_handler = CommandHandler()
+
+    def load_plugins(self):
+        # Dynamically load all plugins in the plugins directory
+        plugins_package = 'app.plugins'
+        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+            if is_pkg:  # Ensure it's a package
+                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                for item_name in dir(plugin_module):
+                    item = getattr(plugin_module, item_name)
+                    try:
+                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
+                            self.command_handler.register_command(plugin_name, item())
+                    except TypeError:
+                        continue  # If item is not a class or unrelated class, just ignore
+    def start(self):
+        # Register commands here
+        self.load_plugins()
+        print("Type 'exit' to exit.")
+        while True:  #REPL Read, Evaluate, Print, Loop
+            self.command_handler.execute_command(input(">>> ").strip())
+            command_parts = input_command.split(maxsplit=1)  # Split into command and arguments
+            command_name = command_parts[0]  # The command itself
+            args = command_parts[1] if len(command_parts) > 1 else ""  # Arguments, if any
+
 
 
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from app import App
 
 if __name__ == "__main__":
     #App.start()
-    app = App()  # Create an instance of the App class
-    app.start()  # Start the app by calling the start method
+    app = App().start()  # Create an instance of the App class
+    #app.start()  # Start the app by calling the start method
